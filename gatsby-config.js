519821d30b4be24d4f817def0f1b344fc0c52638
JulianG <juliangaramendy@gmail.com>
2019-02-03 08:03:06 +0000

    changing name in config

--- gatsby-config.js
+++ gatsby-config.js
@@ -58,8 +58,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `Banana Gists`,
+        short_name: `Banana Gists`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
