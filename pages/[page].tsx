919a24ed8ebb0db41ab50714e23b582c1a452f01
JulianG <juliangaramendy@gmail.com>
2020-12-10 21:46:37 +0000

    Fix build step

--- pages/[page].tsx
+++ pages/[page].tsx
@@ -1,14 +1,18 @@
-import React from 'react';
-import { GetStaticPaths, GetStaticProps } from "next";
-import { CommonHead, Footer, Navigation } from '../components';
-import { getPage } from '../lib/fs-api';
+import React from 'react'
+import { GetStaticPaths, GetStaticProps } from 'next'
+import { CommonHead, Footer, Navigation } from '../components'
+import { getPage } from '../lib/fs-api'
 
-export const Page = ({ title, description, bodyHtml }: { title: string, description: string, bodyHtml: string }): JSX.Element => {
+type Props = { title: string; description: string; bodyHtml: string }
 
+export const Page = ({ title, description, bodyHtml }: Props): JSX.Element => {
   return (
     <article>
       <header>
-        <CommonHead title={`${title} - Julian​Garamendy​.dev`} description={description} />
+        <CommonHead
+          title={`${title} - Julian​Garamendy​.dev`}
+          description={description}
+        />
         <h1>Julian​Garamendy​.dev</h1>
         <Navigation />
       </header>
@@ -17,28 +21,25 @@ export const Page = ({ title, description, bodyHtml }: { title: string, descript
       </section>
       <Footer />
     </article>
-  );
-};
+  )
+}
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
   const page = context.params?.page
 
   try {
-  const {title, bodyHtml} = await getPage(`${page}`)
-  return { props: {title, bodyHtml}, revalidate: 1 };
-  } catch(e) {
-    return {notFound: true}
+    const { title, bodyHtml } = await getPage(`${page}`)
+    return { props: { title, description: '', bodyHtml }, revalidate: 1 }
+  } catch (e) {
+    return { notFound: true }
   }
-};
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: ['info', 'work'].map(page => (
-      { params: { page } }
-    )),
-    fallback: true
-  };
+    paths: ['info', 'work'].map((page) => ({ params: { page } })),
+    fallback: true,
+  }
 }
 
-
-export default Page;
+export default Page
--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -1,19 +1,26 @@
-import { GetStaticProps, GetStaticPaths } from "next";
-import React from "react";
-import dayjs from "dayjs";
-import { Navigation, CommonHead, Footer } from "../../components";
-import { getAllPosts, getPostBySlug } from "../../lib/merged-api";
-import { Post } from '../../lib/types';
+import { GetStaticProps, GetStaticPaths } from 'next'
+import React from 'react'
+import dayjs from 'dayjs'
+import { Navigation, CommonHead, Footer } from '../../components'
+import { getAllPosts, getPostBySlug } from '../../lib/merged-api'
+import { Post } from '../../lib/types'
 
-type Props = { post: Partial<Post> }
+type Props = { post?: Partial<Post> }
 
 export const BlogPostPage = ({ post }: Props): JSX.Element => {
-  const publishDate = dayjs(post.date).format("D MMMM, YYYY");
+  if (!post) {
+    return <></>
+  }
+
+  const publishDate = dayjs(post.date).format('D MMMM, YYYY')
 
   return (
     <article>
       <header>
-        <CommonHead title={`${post.title} - Julian​Garamendy​.dev`} description={post.description} />
+        <CommonHead
+          title={`${post.title} - Julian​Garamendy​.dev`}
+          description={post.description}
+        />
         <h1>Julian​Garamendy​.dev</h1>
         <Navigation />
       </header>
@@ -25,27 +32,29 @@ export const BlogPostPage = ({ post }: Props): JSX.Element => {
       </section>
       <Footer />
     </article>
-  );
-};
+  )
+}
 
 export const getStaticProps: GetStaticProps<Props> = async (context) => {
   const slug = context.params?.slug
+
   try {
-    const post = await getPostBySlug(`${slug}`);
-    return { props: { post }, revalidate: 1 };
+    const post = await getPostBySlug(`${slug}`)
+    return { props: { post }, revalidate: 1 }
   } catch (e) {
     return { notFound: true }
- }
-};
-
-export const getStaticPaths: GetStaticPaths = async () => {
-  const posts = await getAllPosts();
-  return {
-    paths: posts.map(post => (
-      { params: { slug: post.slug } }
-    )),
-    fallback: true
-  };
+  }
 }
 
-export default BlogPostPage;
+export const getStaticPaths: GetStaticPaths = async () => {
+  const posts = await getAllPosts()
+
+  const paths = posts.map((post) => ({ params: { slug: post.slug } }))
+
+  return {
+    paths,
+    fallback: true,
+  }
+}
+
+export default BlogPostPage
