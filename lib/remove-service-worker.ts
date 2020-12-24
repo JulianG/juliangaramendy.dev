fef3a544a5207359d6928d7dafb5baf78fac0bbd
JulianG <juliangaramendy@gmail.com>
2020-12-24 16:19:27 +0400

    Remove console.log

--- lib/remove-service-worker.ts
+++ lib/remove-service-worker.ts
@@ -1,12 +1,8 @@
 export function removeServiceWorker() {
   if (typeof window !== 'undefined') {
-    console.log('removing service worker:')
     if ('serviceWorker' in window.navigator) {
-      console.log('  found serviceWorker in window.navigator')
       window.navigator.serviceWorker.ready.then((registration) => {
-        console.log('  unregistering service worker')
         registration.unregister()
-        console.log('  done')
       })
     }
   }
