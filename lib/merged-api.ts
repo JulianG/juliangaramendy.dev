a9549499a56d09ae317dc8cfead13b8265110298
JulianG <juliangaramendy@gmail.com>
2021-01-22 17:59:50 +0000

    disabling fs-api

--- lib/merged-api.ts
+++ lib/merged-api.ts
@@ -1,16 +1,13 @@
 import { Post, PostSummary } from './types'
-import {
-  getPostBySlug as getFSPostBySlug,
-  getAllPosts as getAllFSPosts,
-} from './fs-api'
+import { getPostBySlug as getFSPostBySlug } from './fs-api'
 import {
   getPostBySlug as getDevPostBySlug,
   getAllPosts as getAllDevPosts,
 } from './dev-api'
 
 export async function getAllPosts(): Promise<PostSummary[]> {
-  const [fs, dev] = await Promise.all([getAllFSPosts(), getAllDevPosts()])
-  const allPosts = [...fs, ...dev].sort((post1, post2) =>
+  const [dev] = await Promise.all([getAllDevPosts()])
+  const allPosts = [...dev].sort((post1, post2) =>
     post1.date > post2.date ? -1 : 1
   )
   return allPosts
